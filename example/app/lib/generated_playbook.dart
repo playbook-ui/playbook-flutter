a5f31b8383bfed194d19b61800958653041d0804
Mao <lcdsmao@gmail.com>
2021-09-13 10:39:42 +0900

    chore: run build runner

--- example/app/lib/generated_playbook.dart
+++ example/app/lib/generated_playbook.dart
@@ -9,12 +9,6 @@ _i1.Playbook get playbook => _i1.Playbook(stories: stories);
 List<_i1.Story> get stories => [_$foo$foo_widget$Story(), _$bar$bar$Story()];
 _i1.Story _$foo$foo_widget$Story() => _i1.Story('FooWidget', scenarios: [
       _i1.Scenario(
-        'foo5',
-        layout: _i2.ScenarioLayout.compressed(),
-        scale: 0.3,
-        child: _i3.$foo5(),
-      ),
-      _i1.Scenario(
         'foo3foo3',
         layout: _i2.ScenarioLayout.sizing(
             _i2.ScenarioLayoutFixed(100.0), _i2.ScenarioLayoutFixed(200.0)),
@@ -26,6 +20,12 @@ _i1.Story _$foo$foo_widget$Story() => _i1.Story('FooWidget', scenarios: [
         layout: _i2.ScenarioLayout.compressed(),
         scale: 0.3,
         child: _i3.$Foo4_Bar(),
+      ),
+      _i1.Scenario(
+        'foo5',
+        layout: _i2.ScenarioLayout.compressed(),
+        scale: 0.3,
+        child: _i3.$foo5(),
       ),
       _i3.foo1(),
       _i3.foo2()
