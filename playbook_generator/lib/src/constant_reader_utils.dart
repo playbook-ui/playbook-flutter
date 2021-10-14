1b73144d3dc6c1e5c644ad738858b1ea5485062d
Mao Yufeng <lcdsmao@gmail.com>
2021-10-14 11:07:07 +0900

    chore: remove unused import

--- playbook_generator/lib/src/constant_reader_utils.dart
+++ playbook_generator/lib/src/constant_reader_utils.dart
@@ -1,6 +1,5 @@
 import 'package:code_builder/code_builder.dart';
 import 'package:source_gen/source_gen.dart';
-import 'package:dartx/dartx.dart';
 
 /// Currently only for ScenarioLayout
 String constantReaderToSource(
