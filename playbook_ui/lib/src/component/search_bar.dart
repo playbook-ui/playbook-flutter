8958948344cc91fb13b278fa2dcbfbfe54716452
Mao <lcdsmao@gmail.com>
2021-12-04 22:37:32 +0900

    feat: adjust search bar color

--- playbook_ui/lib/src/component/search_bar.dart
+++ playbook_ui/lib/src/component/search_bar.dart
@@ -30,34 +30,34 @@ class _SearchBarState extends State<SearchBar> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final iconColor = theme.colorScheme.onSurface.withOpacity(0.48);
     return Material(
       shape: const StadiumBorder(),
       color: theme.colorScheme.onSurface.withOpacity(0.06),
       clipBehavior: Clip.antiAlias,
-      child: Theme(
-        data: theme.copyWith(
-          // Change prefixIcon/suffixIcon active color
-          colorScheme: theme.colorScheme.copyWith(
-            primary: theme.disabledColor,
+      child: TextField(
+        controller: widget.controller,
+        keyboardType: TextInputType.text,
+        style: theme.textTheme.headline6,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.all(16),
+          prefixIcon: Icon(
+            Icons.search,
+            size: 32,
+            color: iconColor,
           ),
-        ),
-        child: TextField(
-          controller: widget.controller,
-          keyboardType: TextInputType.text,
-          cursorColor: theme.primaryColor,
-          style: theme.textTheme.headline6,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            contentPadding: const EdgeInsets.all(16),
-            prefixIcon: const Icon(Icons.search, size: 32),
-            suffixIcon: _hasText
-                ? IconButton(
-                    onPressed: () => widget.controller.clear(),
-                    icon: const Icon(Icons.cancel, size: 24),
-                  )
-                : null,
-            hintText: 'Search',
-          ),
+          suffixIcon: _hasText
+              ? IconButton(
+                  onPressed: () => widget.controller.clear(),
+                  icon: Icon(
+                    Icons.cancel,
+                    size: 24,
+                    color: iconColor,
+                  ),
+                )
+              : null,
+          hintText: 'Search',
         ),
       ),
     );
