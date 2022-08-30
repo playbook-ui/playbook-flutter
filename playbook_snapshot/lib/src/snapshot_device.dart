a6ecf1a4af775bb14281ed433c501910260b18b3
b4tchkn <baaaakkrad@gmail.com>
2022-08-30 17:29:35 +0900

    refactor: keep some devices

--- playbook_snapshot/lib/src/snapshot_device.dart
+++ playbook_snapshot/lib/src/snapshot_device.dart
@@ -11,4 +11,34 @@ class SnapshotDevice {
   final String name;
   final Size size;
   final TargetPlatform platform;
+
+  static const iPhoneSE = SnapshotDevice(
+    name: 'iPhoneSE',
+    size: Size(320, 568),
+    platform: TargetPlatform.iOS,
+  );
+
+  static const iPhone8 = SnapshotDevice(
+    name: 'iPhone8',
+    size: Size(375, 667),
+    platform: TargetPlatform.iOS,
+  );
+
+  static const iPhone11 = SnapshotDevice(
+    name: 'iPhone11',
+    size: Size(414, 896),
+    platform: TargetPlatform.iOS,
+  );
+
+  static const pixel4 = SnapshotDevice(
+    name: 'pixel4',
+    size: Size(411, 869),
+    platform: TargetPlatform.android,
+  );
+
+  static const xperia = SnapshotDevice(
+    name: 'xperia',
+    size: Size(360, 640),
+    platform: TargetPlatform.android,
+  );
 }
