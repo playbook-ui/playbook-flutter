0744b5c11887c25b2d499c2fa9bd7727433289e7
b4tchkn <baaaakkrad@gmail.com>
2022-09-01 12:36:17 +0900

    fix: test device

--- examples/generator_example/generator_catalog_app/test/snapshot_test.dart
+++ examples/generator_example/generator_catalog_app/test/snapshot_test.dart
@@ -9,11 +9,7 @@ Future<void> main() async {
       Snapshot(
         directoryPath: 'screenshots',
         devices: [
-          SnapshotDevice(
-            name: 'iPhone8',
-            size: Size(375, 667),
-            platform: TargetPlatform.iOS,
-          ),
+          SnapshotDevice.iPhone8,
         ],
       ),
       tester,
